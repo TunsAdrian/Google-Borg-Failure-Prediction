2bc9f79a8b65fd7d7806e372c545d4ef49d7f687
Adrian-Ioan Tuns <50252048+TunsAdrian@users.noreply.github.com>
2023-06-16 16:27:58 +0300

    added the data preprocessing
    made the following preprocessing steps on the dataset:
    - removed jobs and tasks not in the timeframe
    - aggregated CPU and Memory resource request from tasks to jobs
    - removed intermediary states and mapped the ones left to success and failure
    - found out the job parent-child dependency and removed the jobs and tasks that failed after the parent (because the parent job sent a kill signal to its child jobs)

--- main.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-datasetRootPath = "D:\\Documents\\Programming\\Python-Projects\\Clusterdata_2019_e\\"
-spark = SparkSession.builder.appName('Google Borg Cluster Traces Analysis').master('local[*]').getOrCreate()
-
-parDF = spark.read.parquet(datasetRootPath + "instance_usage-*.parquet.gz")
-parDF.createOrReplaceTempView("ParquetTable")
-spark.sql("SELECT * FROM ParquetTable").show(n=50)
